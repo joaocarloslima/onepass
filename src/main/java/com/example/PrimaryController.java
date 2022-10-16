7779611186f059e1a06768f2148ce778fec326f8
Joao <joaocarloslima@me.com>
2022-10-16 10:00:21 -0300

    feat: insert de senha

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,20 +1,25 @@
 package com.example;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.example.conexao.ConnectionFactory;
+import com.example.model.Acesso;
+import com.example.model.dao.AcessoDao;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
+    @FXML TextField textFieldLocal;
+    @FXML TextField textFieldLogin;
+    @FXML TextField passwordFieldSenha;
+
     public void salvar(){
+        
         try {
-            Connection connection = ConnectionFactory.getConnection();
-            connection.close();
-            System.out.println("conectado");
+            new AcessoDao().salvar(carregarAcessoDoForm());
         } catch (SQLException e) {
             mostrarMensagem("Erro ao conectar com o banco de dados");        
             e.printStackTrace();
@@ -26,6 +31,14 @@ public class PrimaryController {
         alert.setTitle("Erro");
         alert.setContentText(mensagem);
         alert.showAndWait();
+    }
+
+    private Acesso carregarAcessoDoForm(){
+        var acesso = new Acesso();
+        acesso.setLocal(textFieldLocal.getText());
+        acesso.setLogin(textFieldLogin.getText());
+        acesso.setSenha(passwordFieldSenha.getText());
+        return acesso;
     }
 
 
---
+++ src/main/java/com/example/model/Acesso.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+public class Acesso {
+
+    private String local;
+    private String login;
+    private String senha;
+
+    public Acesso() {
+    }
+
+    public Acesso(String local, String login, String senha) {
+        this.local = local;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Acesso [local=" + local + ", login=" + login + ", senha=" + senha + "]";
+    }
+
+}
