52182cbe1c6db8a0034abea4ab6513be588c5d6d
Joao <joaocarloslima@me.com>
2022-10-16 09:43:19 -0300

    feat: conexao com banco

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,33 @@
 package com.example;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.example.conexao.ConnectionFactory;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void salvar(){
+        try {
+            Connection connection = ConnectionFactory.getConnection();
+            connection.close();
+            System.out.println("conectado");
+        } catch (SQLException e) {
+            mostrarMensagem("Erro ao conectar com o banco de dados");        
+            e.printStackTrace();
+        }
     }
+
+    private void mostrarMensagem(String mensagem) {
+        var alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Erro");
+        alert.setContentText(mensagem);
+        alert.showAndWait();
+    }
+
+
+
 }
